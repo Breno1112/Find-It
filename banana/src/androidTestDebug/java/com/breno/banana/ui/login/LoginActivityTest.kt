48608e702838875b1672b4456cbbbb8701bd484c
AwsHelper <awshelper@iamahelper.com>
2020-01-11 18:07:26 -0300

    launching module activity

--- banana/src/androidTestDebug/java/com/breno/banana/ui/login/LoginActivityTest.kt
+++ banana/src/androidTestDebug/java/com/breno/banana/ui/login/LoginActivityTest.kt
@@ -1,9 +1,14 @@
 package com.breno.banana.ui.login
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
 import org.junit.Test
 
 
 class LoginActivityTest {
+
+    @get:Rule
+    val rule = ActivityScenarioRule<LoginActivity>(LoginActivity::class.java)
 
     @org.junit.Before
     fun setUp() {
