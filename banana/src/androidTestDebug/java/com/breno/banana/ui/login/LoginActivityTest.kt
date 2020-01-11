d6dd23fe3d19c19f50431f57b931e02e92020944
AwsHelper <awshelper@iamahelper.com>
2020-01-11 17:57:14 -0300

    starting with tests ok

---
+++ banana/src/androidTestDebug/java/com/breno/banana/ui/login/LoginActivityTest.kt
@@ -0,0 +1,20 @@
+package com.breno.banana.ui.login
+
+import org.junit.Test
+
+
+class LoginActivityTest {
+
+    @org.junit.Before
+    fun setUp() {
+    }
+
+    @org.junit.After
+    fun tearDown() {
+
+    }
+
+    @Test
+    fun test(){
+    }
+}
---
+++ banana/src/main/java/com/breno/banana/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.breno.banana.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
