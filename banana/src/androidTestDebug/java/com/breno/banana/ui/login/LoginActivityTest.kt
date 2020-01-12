335a213a2f2c0dec47f23432047fc22d94acbd5a
AwsHelper <awshelper@iamahelper.com>
2020-01-12 01:07:12 -0300

    implementando testes de login

--- banana/src/androidTestDebug/java/com/breno/banana/ui/login/LoginActivityTest.kt
+++ banana/src/androidTestDebug/java/com/breno/banana/ui/login/LoginActivityTest.kt
@@ -1,14 +1,34 @@
 package com.breno.banana.ui.login
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import com.breno.banana.R
+import org.hamcrest.CoreMatchers.not
 import org.junit.Rule
 import org.junit.Test
 
 
 class LoginActivityTest {
 
+    fun isKeyboardShown(): Boolean {
+        val inputMethodManager = InstrumentationRegistry.getInstrumentation().targetContext.getSystemService(
+            Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        return inputMethodManager.isAcceptingText
+    }
+
+    fun sendKeys(keys: String){
+        val keySender = InstrumentationRegistry.getInstrumentation()
+        keySender.sendStringSync(keys)
+    }
+
     @get:Rule
-    val rule = ActivityScenarioRule<LoginActivity>(LoginActivity::class.java)
+    val activityRule = ActivityTestRule(LoginActivity::class.java)
 
     @org.junit.Before
     fun setUp() {
@@ -16,10 +36,28 @@ class LoginActivityTest {
 
     @org.junit.After
     fun tearDown() {
+        activityRule.finishActivity()
 
     }
 
     @Test
-    fun test(){
+    fun layoutTest(){
+        onView(withId(R.id.username)).check(matches(isDisplayed()))
+        onView(withId(R.id.password)).check(matches(isDisplayed()))
+        onView(withId(R.id.login)).check(matches(isDisplayed()))
+        onView(withId(R.id.loading)).check(matches(not(isDisplayed())))
+    }
+
+    @Test
+    fun loginSimulator(){
+        onView(withId(R.id.username)).perform(click())
+        check(isKeyboardShown())
+        sendKeys("este teste instrumental funciona perfeitamente em um modulo")
+        onView(withId(R.id.username)).check(matches(withText("este teste instrumental funciona perfeitamente em um modulo")))
+        onView(withId(R.id.password)).perform(click())
+        sendKeys("senha")
+        onView(withId(R.id.password)).check(matches(withText("senha")))
+        onView(withId(R.id.login)).perform(click())
+        check(isKeyboardShown())
     }
 }
